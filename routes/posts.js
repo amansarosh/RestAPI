1b8f84fbc8b4b20b65431fd96aeba99e19131fbd
Aman Sarosh <amansarosh13@gmail.com>
2020-05-31 18:36:05 -0700

    first commit

---
+++ routes/posts.js
@@ -0,0 +1,51 @@
+const express = require('express');  // Import the package
+const router = express.Router();
+const Post = require('../models/Post');
+
+// Get all posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts)
+    } catch{
+        res.json({ message: err })
+    }
+});
+
+// Submits a post
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    const savedPost = await post.save();
+    res.json(savedPost)
+})
+
+// Get back specific post
+router.get('/:postId', async (req, res) => {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+});
+
+// Delete pots
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({ _id: req.params.postId })
+        res.json(removedPost);
+    } catch{
+        res.json({ message: err })
+    }
+});
+
+// Update a post
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatedPost = post.updateOne({ _id: req.params.postId }, { $set: { title: req.body.title } })
+        res.json(updatedPost)
+    } catch{
+        res.json({ message: err })
+    }
+})
+
+module.exports = router;
